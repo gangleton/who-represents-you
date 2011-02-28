10d96bde808cb6dc7303183945de5e689813fab0
Serena <serenawr@gmail.com>
2011-02-28 17:48:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SunlightPlayground::Application.config.secret_token = '0bf81bd96ce7ed81f04ebc744d3ab7958e64c419a42c24a91b8629c867384e67f267fed4bbb6c24a8bc75d93981a0bc931121d29a002c37865994aab1d3eed02'
---
+++ config/initializers/sunlight.rb
@@ -0,0 +1,4 @@
+require 'rubygems'
+require 'sunlight'
+Sunlight::Base.api_key = '09555634b6314eb7853090da5bdccbcd'
+
